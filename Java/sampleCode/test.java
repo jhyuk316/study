45db566398302e1b286feb9d5f720998e0026b11
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2021-12-16 21:55:44 +0900

    study java

--- Java/sampleCode/test.java
+++ Java/sampleCode/test.java
@@ -18,14 +18,14 @@ class NewClass {
 
 
 class test {
-    @SuppressWarnings("deprecation") // deprecation관련 경고를 억제
+    // @SuppressWarnings("deprecation") // deprecation관련 경고를 억제
     public static void main(String args[]) {
         NewClass nc = new NewClass();
 
         nc.oldField = 10; // @Depreacted가 붙은 대상을 사용
         System.out.println(nc.getOldField()); // @Depreacted가 붙은 대상을 사용
 
-        // @SuppressWarnings("unchecked") // 지네릭스 관련 경고를 억제
+        @SuppressWarnings("unchecked") // 지네릭스 관련 경고를 억제
         ArrayList<NewClass> list = new ArrayList(); // 타입을 지정하지 않음.
         list.add(nc);
 
